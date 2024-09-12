e3779c53d70ba5d3d4ee793c496e71e89ab13777
plries <apaulries@gmail.com>
2024-09-11 19:03:19 -0700

    hover link animation

--- components/footer.php
+++ components/footer.php
@@ -8,13 +8,13 @@
                     </div>
                     <a href="mailto:hello@paularies.ca" class="email flex row aic gap-8" tabindex="0">
                         <img src="./icons/ui/black-mail.svg" alt="Email icon." draggable="false">
-                        <p>info@paularies.ca</p>
+                        hello@paularies.ca
                     </a>
                 </article>
                 <div class="footer-links flex col gap-8">
-                    <a href="https://www.linkedin.com/in/paularies/" target="_blank">linkedin</a>
-                    <a href="https://github.com/plries" target="_blank">github</a>
-                    <a href="https://www.instagram.com/plries/" target="_blank">instagram</a>
+                    <a class="link-underline" href="https://www.linkedin.com/in/paularies/" target="_blank">linkedin</a>
+                    <a class="link-underline" href="https://github.com/plries" target="_blank">github</a>
+                    <a class="link-underline" href="https://www.instagram.com/plries/" target="_blank">instagram</a>
                 </div>
                 <a href="#" class="footer-logo">
                     <img src="./icons/ui/logo-big.svg" alt="Paul Aries Logo." draggable="false">
