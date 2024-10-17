131f9c0bc196ce737eee3d4fa0e134ed0bd561f9
plries <apaulries@gmail.com>
2024-10-16 22:04:58 -0700

    image hover

--- js/tippy.js
+++ js/tippy.js
@@ -5,3 +5,15 @@ tippy('.skill', {
 tippy('.code-dropdown-btn', {
     placement: 'top',
 });
+
+tippy('#outdoors' , {
+    content: "<div style='display: grid; place-items: center; width: 100%; height: 100%; gap: 8px; padding: 4px 0px;'><img src='./media/tippy/outdoors.jpg' style='width: 100%; height: 100%; border-radius: 8px;'></div>",
+    allowHTML: true,
+    placement: 'top',
+});
+
+tippy('#concert' , {
+    content: "<div style='display: grid; place-items: center; width: 100%; height: 100%; gap: 8px; padding: 4px 0px;'><img src='./media/tippy/concert.jpg' style='width: 100%; height: 100%; border-radius: 8px;'></div>",
+    allowHTML: true,
+    placement: 'top',
+});
