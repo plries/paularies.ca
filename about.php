92ac35356229c0d39f5abc8741f8ddd2fcf7c8b0
plries <apaulries@gmail.com>
2024-11-21 12:24:33 -0800

    updated css icon +  gradient

--- about.php
+++ about.php
@@ -101,10 +101,10 @@
             <article class="cell skills span-lg flex col gap-16 about-reveal">
                 <h3>my skills</h3>
                 <div class="skills-container flex row gap-32">
-                    <img class="skill" src="./icons/skills/html5.svg" alt="HTML5 icon." draggable="false"
+                    <img class="skill" src="./icons/skills/html.svg" alt="HTML icon." draggable="false"
                         data-tippy-content="HTML5">
-                    <img class="skill" src="./icons/skills/css3.svg" alt="CSS3 icon." draggable="false"
-                        data-tippy-content="CSS3">
+                    <img class="skill" src="./icons/skills/css.svg" alt="CSS icon." draggable="false"
+                        data-tippy-content="CSS">
                     <img class="skill" src="./icons/skills/javascript.svg" alt="JavaScript icon."
                         data-tippy-content="JavaScript">
                     <img class="skill" src="./icons/skills/react.svg" alt="React icon." draggable="false"
